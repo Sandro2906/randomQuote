9795f9f9aa76ddb984c5444946bdc5d6e7be45f2
Sandro Gataric <132219558+Sandro2906@users.noreply.github.com>
2023-08-24 20:53:36 +0200

    Update NavBar.jsx
    Chnaged mistakes.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NavBar = () => {
   return (
     <div className='h-auto w-full bg-blue-300 flex justify-around items-center py-7'>
-        <div><h1 className='text-4xl font-extrabold'>RANDOM QOUTES</h1></div>
+        <div><h1 className='text-4xl font-extrabold'>RANDOM QUOTES</h1></div>
         <div><h1 className='text-1xl'>This site has been made by Sandro Gataric.</h1></div>
     </div>
   )
